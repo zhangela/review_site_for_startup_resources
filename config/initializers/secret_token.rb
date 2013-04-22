348e7014a2e50e6739aad3f0c84a4bdb295cda39
Angela Zhang <angelaz@mit.edu>
2013-04-22 17:26:06 -0400

    restarted rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Azure::Application.config.secret_token = '1eb98486511da6d8c14a4fedf2954ef624f38ddbdf7e5b346c12fce12cecfce108541334867540547e4a8e9e88fc9970491fe6314c2b467cb065fb1c9a487c0b'
