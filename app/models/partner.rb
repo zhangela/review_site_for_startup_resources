8e91cfb26377e3dc286ea3ed3fe63890808387e1
jmello <jmmello@mit.edu>
2013-04-23 16:26:46 -0400

    added company_id as attr_accessible to partners

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,7 +2,7 @@ class Partner < ActiveRecord::Base
     belongs_to :company
     has_many :reviews, :as => :reviewable
 
-    attr_accessible :avg_rating, :name
+    attr_accessible :avg_rating, :name, :company_id
 
     before_save :default_values
 
