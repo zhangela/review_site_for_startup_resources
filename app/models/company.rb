fc4960a06c5d720be31f18a025971171eb72fcaa
jmello <jmmello@mit.edu>
2013-04-27 17:12:01 -0400

    star rating

--- app/models/company.rb
+++ app/models/company.rb
@@ -11,11 +11,13 @@ class Company < ActiveRecord::Base
 
     #called whenever a new review is submitted
     def recalculate_average(review)
-
+        puts "1111111111111111111111111"
     	#if no reviews have been submitted
     	if(self.avg_rating == -1)
+            puts "222222222222222222222222222222"
     		self.update_attribute(:avg_rating, review.rating)
     	else
+            puts "3333333333333333333333333333"
 			oldAvg = self.avg_rating 
     		numRatings = self.reviews.size
 			oldTotal = oldAvg * (numRatings-1)
