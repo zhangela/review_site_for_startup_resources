0223c8ad47600746da738efce8514f0c9c4acf2b
jmello <jmmello@mit.edu>
2013-04-27 18:33:27 -0400

    added Comment and deleted DiscussionThread scaffolds

--- db/migrate/20130427221507_create_discussion_threads.rb
+++
@@ -1,8 +0,0 @@
-class CreateDiscussionThreads < ActiveRecord::Migration
-  def change
-    create_table :discussion_threads do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/unit/helpers/discussion_threads_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DiscussionThreadsHelperTest < ActionView::TestCase
-end
