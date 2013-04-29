a0d093084655ead7d377266b12c07108ddb4b3cf
Victoria Li <victoli.d@gmail.com>
2013-04-28 20:38:58 -0400

    deploy

--- db/migrate/20130428042943_change_type_to_private_in_discussion.rb
+++ db/migrate/20130428042943_change_type_to_private_in_discussion.rb
@@ -1,6 +1,6 @@
 class ChangeTypeToPrivateInDiscussion < ActiveRecord::Migration
   def change
-  	remove_column :discussions, :type, :string
+  	
   	add_column :discussions, :private, :boolean
   end
 end
