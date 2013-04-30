c1b1558f061725f5501a9d0126d4fd072abc3894
jmello <jmmello@mit.edu>
2013-04-30 15:55:52 -0400

    work on only allowing mit.eu email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ActiveRecord::Base
   # :token_authenticatable,
   #  :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable # :confirmable
+         :recoverable, :rememberable, :trackable#, :validatable # :confirmable
+  #validates_format_of :email, :with => /(.*)@mit.edu/i, :message => "Must be mit.edu email address"
   has_many :reviews
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :name, :remember_me
-  # attr_accessible :title, :body
+
 end
