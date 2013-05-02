2d3ef92457ef56ddb4f492daef80adb24cbf7715
jmello <jmmello@mit.edu>
2013-05-02 14:56:16 -0400

    fixed user conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable # :confirmable
   validates_format_of :email, :with => /(.*)@mit.edu/i, :message => "Must be mit.edu email address"
   has_many :reviews
-<<<<<<< HEAD
   has_many :comments
-
-=======
   has_one :profile
->>>>>>> 13cc93aacd93d8e5f6c7739534df6ce8596daa50
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :name, :remember_me, :avatar
   # attr_accessible :title, :body
