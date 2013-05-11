942eb03fe0a459f41d1216cc4e0973a1a235e7b5
Angela Zhang <angelaz@mit.edu>
2013-05-11 15:49:43 -0400

    commented comments controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # calls the notifications method to load notifications in the navbar
   before_filter :notifications
+
   helper_method :count
 
   private
+
+  	# loads relevant notifications for the current_user in the navbar
 	def notifications
 		@num_unread = 0
 		if current_user
@@ -12,9 +16,11 @@ class ApplicationController < ActionController::Base
 		  @relevant_notifications = []
 
 		  @notifications.each do |notification|
+
+		  	# only shows notifications not caused by current user
 		  	if notification.user_id != current_user.id
-		  		puts notification.user_id
-		  		puts current_user.id
+
+		  		# loads the list of relevant users (who should be notified) for this notification
 			  	@relevant_users = []
 			  	@review = Review.find(notification.review_id)
 			  	@review.discussions.each do |discussion|
@@ -22,10 +28,13 @@ class ApplicationController < ActionController::Base
 			  			@relevant_users << comment.user_id
 			  		end
 			  	end
-			  	if @relevant_users.include? current_user.id 
+
+			  	# only shows notifications that are relevant to the current_user
+			  	# relavance is defined by the user having commented on a particular discussion
+			  	if @relevant_users.include? current_user.id
 				  		@relevant_notifications << notification
 				  		@num_unread = @num_unread + 1
-			  	end 
+			  	end
 			end
 		  end
 		end
